a6b450d89414b66a8f7c85b862e3d517581a7b51
CarlMM <kaptenkeps@gmail.com>
2021-09-09 19:40:19 +0200

    Call kanyewest API

--- kanyeGen/src/App.vue
+++ kanyeGen/src/App.vue
@@ -1,13 +1,36 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <h1>{{ quote }}</h1>
+    <h4>- Kanye West</h4>
+  </div>
 </template>
+
+
+<script>
+
+import axios from 'axios'
+import {ref} from 'vue'
+
+
+export default {
+  setup() {
+
+      const quote = ref('')
+
+      axios.get('https://api.kanye.rest/')
+      .then(response =>{
+        quote.value = response;
+      })
+
+      return {
+        quote
+      }
+  },
+}
+</script>
+
 
 <style>
 #app {
